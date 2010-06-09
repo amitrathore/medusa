a41138176fe5bccd083529eee30503e07ea518e1
Amit Rathore <amitrathore@gmail.com>
2010-06-09 10:58:39 -0700

    basic functionality for a new bounded IO-threadpool

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject medusa "0.1.0"
+  :description "A supervised threadpool for clojure futures"
+  :url "http://github.com/amitrathore/medusa"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [clj-utils "0.1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [leiningen/lein-swank "1.1.0"]])
---
+++ src/org/rathore/amit/medusa/core.clj
@@ -0,0 +1,44 @@
+(ns org.rathore.amit.medusa.core
+  (:use org.rathore.amit.utils.clojure)
+  (:import java.util.concurrent.ExecutorService
+           java.util.concurrent.Executors))
+
+(def THREAD-TIMEOUT-MILLIS 20000)
+(def SUPERVISE-EVERY-MILLIS 10000)
+(def MEDUSA-THREADPOOL-SIZE (* 3 (.availableProcessors (Runtime/getRuntime))))
+(def THREADPOOL (Executors/newFixedThreadPool MEDUSA-THREADPOOL-SIZE))
+
+(def running-futures (ref {}))
+
+(defn claim-thread [future-id]
+  (let [thread-info {:thread (Thread/currentThread) :future-id future-id :started (System/currentTimeMillis)}]
+    (dosync (alter running-futures assoc future-id thread-info))))
+
+(defn mark-completion [future-id]
+  (dosync (alter running-futures dissoc future-id)))
+
+(defn medusa-future [future-id thunk]
+  (let [work (fn []
+               (claim-thread future-id)
+               (thunk))]
+    (.submit THREADPOOL work)))
+
+(defn preempt-medusa-future [[future-id {:keys [thread]}]]
+  (.interrupt thread)
+  (mark-completion future-id))
+
+(defn running-over? [[_ {:keys [started]}]]
+  (> (- (System/currentTimeMillis) started) THREAD-TIMEOUT-MILLIS))
+
+(defn preempt-old-futures []
+  (let [to-preempt (filter running-over? @running-futures)]
+    (doseq [frs to-preempt]
+      (preempt-medusa-future frs))))
+
+(defn supervise [repeat-after-millis]
+  (while true
+    (preempt-old-futures)
+    (Thread/sleep repeat-after-millis)))
+
+(defrunonce start-supervisor []
+  (future (supervise SUPERVISE-EVERY-MILLIS)))
