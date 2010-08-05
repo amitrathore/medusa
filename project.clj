e65a7cc53e42887143732758c513638017513c7a
Amit Rathore <amitrathore@gmail.com>
2010-08-05 13:41:51 -0700

    bumping version number to 0.1.6

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.runa/medusa "0.1.5"
+(defproject org.clojars.runa/medusa "0.1.6"
   :description "A supervised threadpool for clojure futures"
   :url "http://github.com/amitrathore/medusa"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.clojars.runa/clj-utils "0.1.3"]]
+                 [org.clojars.runa/clj-utils "0.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [leiningen/lein-swank "1.1.0"]])
